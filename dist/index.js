29795dcba623cd37b52d478e89e5b8c9a071b666
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2024-08-17 17:15:42 +0530

    fix: add solana/web3 tweetnacl lib

--- dist/index.js
+++ dist/index.js
@@ -1,52 +1,20 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const ed = __importStar(require("@noble/ed25519"));
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Generate a secure random private key
-        const privKey = ed.utils.randomPrivateKey();
-        // Convert the message "hello world" to a Uint8Array
-        const message = new TextEncoder().encode("hello world");
-        // Generate the public key from the private key
-        const pubKey = yield ed.getPublicKeyAsync(privKey);
-        // Sign the message
-        const signature = yield ed.signAsync(message, privKey);
-        // Verify the signature
-        const isValid = yield ed.verifyAsync(signature, message, pubKey);
-        // Output the result
-        console.log(isValid); // Should print `true` if the signature is valid
-    });
-}
-main();
+const web3_js_1 = require("@solana/web3.js");
+const tweetnacl_1 = __importDefault(require("tweetnacl"));
+// Generate a new keypair
+const keypair = web3_js_1.Keypair.generate();
+// Extract the public and private keys
+const publicKey = keypair.publicKey.toString();
+const secretKey = keypair.secretKey;
+// Display the keys
+console.log("Public Key:", publicKey);
+console.log("Private Key (Secret Key):", secretKey);
+// Convert the message "hello world" to a Uint8Array
+const message = new TextEncoder().encode("hello world");
+const signature = tweetnacl_1.default.sign.detached(message, secretKey);
+const result = tweetnacl_1.default.sign.detached.verify(message, signature, keypair.publicKey.toBytes());
+console.log(result);
